8df1c77e496ea53a96fd71ef2c7447b3869f3ee8
desmond kristian <desmondkristian@desmonds-MacBook-Air.local>
2022-08-26 16:26:24 +0800

    add events route

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CareersModule } from './careers/careers.module';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [AuthModule, CareersModule],
+  imports: [AuthModule, CareersModule, EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
