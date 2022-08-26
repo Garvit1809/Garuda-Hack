4456e5d40c141900efe6d7191e9dccd65beb537e
desmond kristian <desmondkristian@desmonds-MacBook-Air.local>
2022-08-26 16:27:03 +0800

    add mentors route

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CareersModule } from './careers/careers.module';
 import { EventsModule } from './events/events.module';
+import { MentorsModule } from './mentors/mentors.module';
 
 @Module({
-  imports: [AuthModule, CareersModule, EventsModule],
+  imports: [AuthModule, CareersModule, EventsModule, MentorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
